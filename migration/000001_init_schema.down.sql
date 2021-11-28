48846fb894b12996142c1f0f5cd9d81d6987cd49
FacuBar <efacubar@gmail.com>
2021-11-27 22:42:07 -0300

    sql schemas

---
+++ migration/000001_init_schema.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `published`;
+
+DROP TABLE IF EXISTS `authorship`;
+
+DROP TABLE IF EXISTS `books`;
+
+DROP TABLE IF EXISTS `authors`;
+
+DROP TABLE IF EXISTS `publishers`;
---
+++ migration/000001_init_schema.up.sql
@@ -0,0 +1,70 @@
+CREATE TABLE `publishers` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `description` TEXT NOT NULL,
+  `slogan` TEXT NOT NULL,
+  `founded` DATE NOT NULL,
+
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `authors` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(100) NOT NULL, 
+  `last_name` VARCHAR(100) NOT NULL,
+  `biography` TEXT NOT NULL,
+  `birthday` DATE NOT NULL,
+  `death` DATE,
+
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `books` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `original_release` DATE NOT NULL,
+  `description` TEXT NOT NULL,
+  `short_description` TEXT NOT NULL,
+
+  `published` DATE NOT NULL,
+  `publisher_id` INT UNSIGNED NOT NULL,
+  `pages`  INT UNSIGNED NOT NULL,
+
+  `seller_id` INT UNSIGNED NOT NULL,
+
+  PRIMARY KEY (`id`),
+  CONSTRAINT `books_constr_publishers`
+    FOREIGN KEY (`publisher_id`) REFERENCES `publishers`(`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE `authorship` (
+  `book_id` INT UNSIGNED NOT NULL,
+  `author_id` INT UNSIGNED NOT NULL,
+
+  PRIMARY KEY (`book_id`, `author_id`),
+
+  CONSTRAINT `authorship_constr_book`
+    FOREIGN KEY (`book_id`) REFERENCES `books`(`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+
+  CONSTRAINT `authorship_constr_author`
+    FOREIGN KEY (`author_id`) REFERENCES `authors`(`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `published` (
+  `author_id` INT UNSIGNED NOT NULL,
+  `publisher_id` INT UNSIGNED NOT NULL, 
+
+  PRIMARY KEY (`author_id`, `publisher_id`),
+
+  CONSTRAINT `published_constr_publisher`
+    FOREIGN KEY (`publisher_id`) REFERENCES `publishers`(`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+
+  CONSTRAINT `published_constr_author`
+    FOREIGN KEY (`author_id`) REFERENCES `authors`(`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+)

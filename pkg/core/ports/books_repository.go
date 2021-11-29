98370651cbe3527981c66d78b6a752dd6df34094
FacuBar <efacubar@gmail.com>
2021-11-28 23:42:33 -0300

    returning pointers instead of "objects"

--- pkg/core/ports/books_repository.go
+++ pkg/core/ports/books_repository.go
@@ -8,13 +8,13 @@ import (
 type BooksRepositoryInterface interface {
 	SaveAuthor(*domain.Author) rest_errors.RestErr
 	UpdateAuthor(*domain.Author) rest_errors.RestErr
-	GetAuthorById(uint32) (domain.AuthorDenormalized, rest_errors.RestErr)
+	GetAuthorById(uint32) (*domain.AuthorDenormalized, rest_errors.RestErr)
 
 	SavePublisher(*domain.Publisher) rest_errors.RestErr
 	UpdatePublisher(*domain.Publisher) rest_errors.RestErr
-	GetPublisherById(uint32) (domain.PublisherDenormalized, rest_errors.RestErr)
+	GetPublisherById(uint32) (*domain.PublisherDenormalized, rest_errors.RestErr)
 
 	SaveBook(*domain.Book) rest_errors.RestErr
-	UpdateBook(*domain.Book)
-	GetBookById(uint32) (domain.BookDenormalized, rest_errors.RestErr)
+	UpdateBook(*domain.Book) rest_errors.RestErr
+	GetBookById(uint32) (*domain.BookDenormalized, rest_errors.RestErr)
 }
